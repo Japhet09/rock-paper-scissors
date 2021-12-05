a887e23f25a47f9d9278173f0a1c95fc1088b143
Japhet09 <japhetalfred19@gmail.com>
2021-12-05 21:24:08 +0100

    moved the solutino from the html to this js file

---
+++ main.js
@@ -0,0 +1,58 @@
+// test to see everything works console.log('hello World') 
+function computerPlay(){
+    const randomNumber = Math.floor(Math.random()*3)
+    if (randomNumber===0){
+        return 'rock'
+    }else if(randomNumber===1){
+        return 'paper'
+    }else{
+        return 'scissors'
+    }
+}
+function computerPlay2(){
+    const randomNumber = Math.floor(Math.random()*3)
+    if (randomNumber===0){
+        return 'rock'
+    }else if(randomNumber===1){
+        return 'paper'
+    }else{
+        return 'scissors'
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    //playerSelection = playerSelection.toLowerCase()
+    if (playerSelection==='rock'&&computerSelection==='paper'){
+        return 'computer win! Paper wraps Rock'
+    }else if(playerSelection==='rock'&&computerSelection==='scissors'){
+        return 'You win Rock crush scissors'
+    }else if(playerSelection==='paper'&&computerSelection==='scissors'){
+        return 'Computer win scissors cut paper'
+    }else if(playerSelection==='paper'&&computerSelection==='rock'){
+        return 'You win paper wraps rock'
+    }else if(playerSelection==='scissors'&&computerSelection==='rock'){
+        return 'Computer win rock  beats scissors'
+    }else if(playerSelection==='scissors'&&computerSelection==='paper'){
+        return 'You win scissor beats paper'
+    }else{
+        return 'It  is a draw'
+    }
+}
+
+//console.log(playRound(playerSel,computerPlay()))
+
+function game(){
+    for(i=0;i<4; i++){
+        let playerSel= prompt('Choose your option: rock, paper or scissors')
+        return console.log(playRound(playerSel,computerPlay()))
+    }
+    /*
+    return console.log(playRound(playerSel,computerPlay()))
+    return console.log(playRound(playerSel,computerPlay()))
+    return console.log(playRound(playerSel,computerPlay()))
+    return console.log(playRound(playerSel,computerPlay()))
+    return console.log(playRound(playerSel,computerPlay()))
+    
+    
+*/
+}
