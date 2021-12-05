4388773d2a77ea82999460e621829bc887805325
Japhet09 <japhetalfred19@gmail.com>
2021-12-06 00:36:14 +0100

    added a simple ui with buttons

--- main.js
+++ main.js
@@ -55,4 +55,24 @@ function game(){
     
     
 */
+
+const button1 = document.createElement('button')
+button1.innerText = 'rock'
+button1.className = 'rock'
+    
+const button2 = document.createElement('button')
+button2.innerText = 'paper'
+button2.className = 'paper'
+
+    
+const button3 = document.createElement('button')
+button3.innerText = 'scissors'
+button3.className = 'scissors'
+    
+const body = document.querySelector('body')
+
+body.appendChild(button1)
+body.appendChild(button2)
+body.appendChild(button3)
+
 }
