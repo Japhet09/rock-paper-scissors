9e95efd9f90a4999a97ec11877630dcb65514d53
Japhet09 <japhetalfred19@gmail.com>
2022-01-02 22:30:26 +0100

    added functionality of the buttons ans saving scores

--- main.js
+++ main.js
@@ -49,12 +49,12 @@ function computerPlay(){
 //* A function that allow playing the game, then storing the result
 
 // Variables to keep score
+let playerScore=0;
+let computerScore=0
 
-let computerScore = 0;
-let playerScore = 0;
 
 function game(){
-    const playerSelection = prompt('Enter your choice:')
+    const playerSelection = button.innerText
     let result = (playRound(playerSelection,computerPlay()))
    
 
@@ -72,17 +72,81 @@ function game(){
 const container = document.querySelector('.container')
 
 const rock = document.createElement('button')
-rock.innerText = 'rock'
+rock.innerText = 'ROCK'
 container.append(rock)
 
 const paper = document.createElement('button')
-paper.innerText = 'paper'
+paper.innerText = 'PAPER'
 container.appendChild(paper)
 
 const scissor = document.createElement('button')
-scissor.innerText = 'scissor'
+scissor.innerText = 'SCISSOR'
 container.appendChild(scissor)
 
+//select all buttons
 const buttons = document.querySelectorAll('button')
 
+
+// display winner
+const div = document.createElement('div')
+div.classList.add('display')
+
+//display score
+const computer = document.createElement('div')
+const player = document.createElement('div')
+
+//counting the score
+const compScore = document.createElement('div')
+compScore.classList.add('score')
+container.appendChild(compScore)
+
+const playScore = document.createElement('div')
+playScore.classList.add('score')
+container.appendChild(playScore)
+
+//Annouce the winner
+const winner = document.createElement('div')
+winner.classList.add('win', 'lose')
+
+
+for(let button of buttons){
+    button.addEventListener('click', function(){
+        const playerSelection = button.innerText
+        let result = (playRound(playerSelection,computerPlay()))
+        div.innerText = result
+        container.appendChild(div)
+
+        //*Keep track of scores
+        let pScore = 0
+        let cScore = 0
+
+        if (result.includes('Win')){
+            pScore +=1
+        }else if(result.includes('Lose')){
+            cScore +=1
+        }
+        computer.innerText =  Number(computer.innerText) +cScore 
+        player.innerText = Number(player.innerText) + pScore
+        compScore.innerText = ` Computer Score: ${computer.innerText}`
+        playScore.innerText = `Your Score:${player.innerText}`
+
+        //Finish the game at score of 5 and restart scores
+        if (computer.innerText >=5 || player.innerText>=5){
+            if(computer.innerText>player.innerText){
+                winner.innerText = 'Game Over You Lost!'
+                container.appendChild(winner)
+            }else{
+               
+                winner.innerText = 'Congratulations! You won Huleee!!!!!!'
+                container.appendChild(winner)
+            }
+            computer.innerText = 0
+            player.innerText = 0
+           
+           
+        }
+
+    })}
+
+    
 
